692b50adb7c3fb95193289d67d04511faefe0770
Spencer Will <spencer.a.will@gmail.com>
2024-01-25 16:13:40 -0500

    Add sidenav component toggled by the menu icon.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SiteHeaderComponent } from './core/site-header/site-header.component';
+import { MatSidenavModule } from '@angular/material/sidenav'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SiteHeaderComponent],
+  imports: [RouterOutlet, SiteHeaderComponent, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
